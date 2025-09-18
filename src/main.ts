4b86e4d9ba234a9f038509cfbf38b3e04a959c82
Lukas2094 <lukas2_8@hotmail.com>
2025-09-17 23:16:45 -0300

    add yaml deploy

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import { IoAdapter } from '@nestjs/platform-socket.io';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+//   app.enableCors({
+//     origin: 'http://localhost:3001',
+//     credentials: true 
+//   });
+
+//   // WebSocket
+//   app.useWebSocketAdapter(new IoAdapter(app));
+
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -7,13 +28,16 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
-    credentials: true 
+    origin: process.env.FRONTEND_URL || '*', // libera pro Render ou pega de variável
+    credentials: true,
   });
 
   // WebSocket
   app.useWebSocketAdapter(new IoAdapter(app));
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000; // Render injeta PORT
+  await app.listen(port, () => {
+    console.log(`🚀 Server is running on port ${port}`);
+  });
 }
 bootstrap();
